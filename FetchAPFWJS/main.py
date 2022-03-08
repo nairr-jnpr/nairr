5df1a029d4f06f9534f44559eeee18ba954811d4
nairr-jnpr <97721810+nairr-jnpr@users.noreply.github.com>
2022-03-09 01:05:44 +0530

    Update main.py

--- FetchAPFWJS/main.py
+++ FetchAPFWJS/main.py
@@ -1,13 +1,16 @@
-from flask import Flask,render_template,jsonify
+from flask import Flask,render_template,jsonify,redirect,url_for
 import requests
 import json
 
 app = Flask(__name__)
 
+headers = ['Name','Zipball Url','Tarball Url']
+rows=[]
+
 @app.route('/')
 def home():
-    user = "mistsys"
     token = "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"
+    user = "mistsys"
     repo = "apfw"
     url = "https://api.github.com/repos/{}/{}/tags".format(user,repo)
     #url = "https://github.com/mistsys/apfw/tags"
@@ -15,19 +18,31 @@ def home():
     headers = {
         'authorization': 'token {}'.format(token)
     }
+
     r = requests.get(url,headers=headers)
 
     js = r.json()
-    #print(type(js))
-    #print(js)
+
     for tag in js:
         #print(type(tag))
         #print(tag['name']+' '+tag['zipball_url'])
         print(tag)
+        row = []
+        row.append(tag['name'])
+        row.append(tag['zipball_url'])
+        row.append(tag['tarball_url'])
+        rows.append(row)
 
-    return jsonify(js)
+    #return jsonify(js)
+    return redirect(url_for('table'))
+
+@app.route('/table')
+def table():
+    return render_template('index.html',headers=headers,rows=rows)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
 
