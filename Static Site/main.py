76e97ef74ad92913e6c24fe883b1cb1617be8195
nairr-jnpr <97721810+nairr-jnpr@users.noreply.github.com>
2022-03-14 14:00:49 +0530

    Update main.py

--- Static Site/main.py
+++ Static Site/main.py
@@ -3,7 +3,8 @@ from flask import Flask,render_template,request
 app = Flask(__name__)
 
 params = []
-headers = ['Label', 'Total Tests', 'Passed', 'Failed']
+headers=['Label','Total Tests','Passed']
+rows = []
 
 @app.route('/')
 def home():
@@ -11,43 +12,20 @@ def home():
 
 @app.route('/json', methods=['POST'])
 def json():
-
+    global rows
+    rows = []
     req = request.get_json()
-    print(req)
-    global params
-    global headers
-    #params = [req['label'], req['total test'], req['passed'], req['failed']]
+    for label in req['labels']:
+        row = [label['label'],label['total_test'],label['passed']]
+        #print(row)
+        rows.append(row)
 
-    try:
-        label = req['label']
-    except:
-        label = '0.0.0'
-    try:
-        tot = req['total test']
-    except:
-        tot = '0'
-
-    try:
-        pasd = req['passed']
-    except:
-        pasd = '0'
-
-    try:
-        fails = req['failed']
-    except:
-        fails = '0'
-
-    params = [label,tot,pasd,fails]
-
-    return "Json posted"
+    return 'Json posted'
 
 
-@app.route('/table')
-def table():
-    if len(params)>0:
-        return render_template('table.html',params=params, headers=headers)
-    else:
-        return render_template('table.html',params=['0.0.0',0,0,0],headers=headers)
+@app.route('/overview')
+def overview():
+    return render_template('overview.html',rows=rows,headers=headers)
 
 if __name__=='__main__':
     app.run(debug=True)
